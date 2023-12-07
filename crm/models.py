a4dcc62c2a67684195e34d7f4a794620b354c85d
YOUR_NAME <YOUR_EMAIL>
2023-12-07 19:30:49 +0200

    comit 07.12.23

--- crm/models.py
+++ crm/models.py
@@ -5,5 +5,12 @@ from django.db import models
 
 class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
-    order_name = models.CharField(max_length= 200)
-    order_phone = models.CharField(max_length=200)
+    order_name = models.CharField(max_length=200, verbose_name="nume")
+    order_phone = models.CharField(max_length=200, verbose_name="numar")
+
+    def __str__(self):
+        return self.order_name
+
+    class Meta:
+        verbose_name = "Comanda"
+        verbose_name_plural = "comenzi"
